cd1a22e631ee88378353c4fb5b1bd75cba5b14d1
An Nguyen <ngtab.an.9@gmail.com>
2018-10-03 11:10:27 +0700

    Add formik fields

---
+++ src/components/InputField.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Field } from 'formik';
+import Input from './Input';
+
+const InputField = ({ name, validate, ...inputProps }) => (
+  <Field
+    name={name}
+    validate={validate}
+    render={({ field, form: { errors, touched } }) => (
+      <Input
+        displayType={errors[name] ? 'error' : 'default'}
+        message={errors[name] && touched[name] ? errors[name] : ''}
+        {...inputProps}
+        {...field}
+      />
+    )}
+  />
+);
+
+InputField.propTypes = {
+  name: PropTypes.string.isRequired,
+  validate: PropTypes.func,
+};
+
+export default InputField;
