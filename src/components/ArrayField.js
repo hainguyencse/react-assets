6759ad710f719de69f520dd087872efc8699cd24
TienHuynh <tien@inspilab.com>
2018-11-08 16:10:20 +0700

    Update array field

--- src/components/ArrayField.js
+++ src/components/ArrayField.js
@@ -7,63 +7,41 @@ import Button from './Button';
 const ArrayField = ({ name, label, arrayData, ...inputProps }) => (
   <FieldArray
     name={name}
-    render={arrayHelpers => (
-      <div className="form-horizontal form-group">
-        <label>{label}</label>
+    render={({arrayHelpers}) => {
+      arrayHelpers.push('');
 
-        {arrayData && arrayData.length > 0 ? (
-          arrayData.map((item, index) => (
-            <div key={index+1} style={{ marginBottom: '10px' }}>
-              <div style={{ width: 'calc(100% - 93px)', display: 'inline-block', marginRight: '10px', verticalAlign: 'top' }}>
-                <Field name={`${name}.${index+1}`} className="form-control" />
-              </div>
-              <Button
-                onClick={() => arrayHelpers.remove(index+1)}
-                displayType={'danger'}
-                data-toogle="tooltip"
-                title={'Remove'}
-              >
-                <i className="fa fa-trash-o" />
-              </Button> &nbsp;
-              <Button
-                onClick={() => arrayHelpers.insert(index+1)}
-                displayType={'primary'}
-                data-toogle="tooltip"
-                title={'Add'}
-              >
-                <i className="fa fa-plus" />
-              </Button>
-            </div>
-          ))
-        ) : null }
+      return (
+        <div className="form-horizontal form-group">
+          <label>{label}</label>
 
-        {arrayData ? (
-          arrayData.map((item, index) => (
-            <div key={index} style={{ marginBottom: '10px' }}>
-              <div style={{ width: 'calc(100% - 93px)', display: 'inline-block', marginRight: '10px', verticalAlign: 'top' }}>
-                <Field name={`${name}.${index}`} className="form-control" />
+          {arrayData && arrayData.length > 0 ? (
+            arrayData.map((item, index) => (
+              <div key={index+1} style={{ marginBottom: '10px' }}>
+                <div style={{ width: 'calc(100% - 93px)', display: 'inline-block', marginRight: '10px', verticalAlign: 'top' }}>
+                  <Field name={`${name}.${index+1}`} className="form-control" />
+                </div>
+                <Button
+                  onClick={() => arrayHelpers.remove(index+1)}
+                  displayType={'danger'}
+                  data-toogle="tooltip"
+                  title={'Remove'}
+                >
+                  <i className="fa fa-trash-o" />
+                </Button> &nbsp;
+                <Button
+                  onClick={() => arrayHelpers.insert(index+1)}
+                  displayType={'primary'}
+                  data-toogle="tooltip"
+                  title={'Add'}
+                >
+                  <i className="fa fa-plus" />
+                </Button>
               </div>
-              <Button
-                onClick={() => arrayHelpers.remove(index)}
-                displayType={'danger'}
-                data-toogle="tooltip"
-                title={'Remove'}
-              >
-                <i className="fa fa-trash-o" />
-              </Button> &nbsp;
-              <Button
-                onClick={() => arrayHelpers.insert(index)}
-                displayType={'primary'}
-                data-toogle="tooltip"
-                title={'Add'}
-              >
-                <i className="fa fa-plus" />
-              </Button>
-            </div>
-          ))
-        ) : null }
-      </div>
-    )}
+            ))
+          ) : null }
+        </div>
+      )  
+    }}
   />
 );
 
