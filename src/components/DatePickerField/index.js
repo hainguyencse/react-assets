a07611e3cd9942b157ad0e6c656af56eba3ab055
TienHuynh <tien@inspilab.com>
2018-11-12 14:53:39 +0700

    Update css of date picker

--- src/components/DatePickerField/index.js
+++ src/components/DatePickerField/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import DatePicker from "react-datepicker";
 import moment from "moment";
 
-import "react-datepicker/dist/react-datepicker.css";
+import "./react-datepicker.css";
 
 export default class DatePickerField extends React.Component {
   constructor(props) {
