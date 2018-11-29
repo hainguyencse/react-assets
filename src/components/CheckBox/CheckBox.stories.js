84b2d909e20a59a54ac69a4de186faa85092a01e
TanNguyen <mntan998@gmail.com>
2018-11-29 15:56:04 +0700

    Update GroupCheckboxField

--- src/components/CheckBox/CheckBox.stories.js
+++ src/components/CheckBox/CheckBox.stories.js
@@ -15,14 +15,19 @@ export default [
       <CheckBox label="Checkbox 2"></CheckBox>
       <CheckBox label="Checkbox 3"></CheckBox>
       <Formik
+        initialValues={{
+          checkbox: ['2'],
+          checkbox6: '2'
+        }}
+        enableReinitialize
         onSubmit={(values) => { console.log('values', values); }}
       >
         {
           ({ isSubmitting, isValid, values }) => (
             <Form>
               <CheckboxField label="Checkbox 4" name="checkbox4" />
-              <GroupCheckboxField label="Checkbox 5" gName="checkbox" name="checkbox5" value="1" />
-              <GroupCheckboxField label="Checkbox 6" gName="checkbox" name="checkbox6" value="2" />
+              <GroupCheckboxField label="Checkbox 5" gName="checkbox" name="checkbox5" value="1" checked={values.checkbox.indexOf('1') >= 0} />
+              <GroupCheckboxField label="Checkbox 6" gName="checkbox" name="checkbox6" value="2" checked={values.checkbox.indexOf('2') >= 0} />
               <br /><br />
               <div style={{ textAlign: 'right' }}>
                 <Button className="btn  btn-primary" type="submit">Generate</Button>
