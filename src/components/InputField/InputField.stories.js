3229649b8bf28ec58c4e28e8f0fbdc957839d1ce
ntban15 <ngtab.an.9@gmail.com>
2019-04-09 17:06:53 +0700

    Add formatter to Input + Add separating by comma functionality for InputField

--- src/components/InputField/InputField.stories.js
+++ src/components/InputField/InputField.stories.js
@@ -28,5 +28,29 @@ export default [
           }
         </Formik>
       </div>
+  },
+  {
+    title: 'Numeric field',
+    component:
+      <div>
+        <Formik
+          onSubmit={(values) => { console.log('values', values); }}
+        >
+          {
+            ({ isSubmitting, isValid, values, setFieldValue }) => (
+              <Form>
+                <InputField
+                  name="numeric-field"
+                  label="Numeric field"
+                  isNumeric
+                />
+                <div style={{ textAlign: 'right' }}>
+                  <Button className="btn btn-primary" type="submit">Submit</Button>
+                </div>
+              </Form>
+            )
+          }
+        </Formik>
+      </div>
   }
 ];
--- src/components/InputField/index.js
+++ src/components/InputField/index.js
@@ -1,34 +1,58 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Field } from 'formik';
+
+// components
 import Input from '../Input';
 
-const InputField = ({ name, validate, ...inputProps }) => (
-  <Field
-    name={name}
-    validate={validate}
-    render={({ field, form: { errors, touched } }) => (
-      <Input
-        displayType={errors[name] ? 'error' : 'default'}
-        message={errors[name] && touched[name] ? errors[name] : ''}
-        {...inputProps}
-        {...field}
-        onChange={(e) => {
-          if (inputProps.onChange) inputProps.onChange(e);
-          field.onChange(e);
-        }}
-        onBlur={(e) => {
-          if (inputProps.onBlur) inputProps.onBlur(e);
-          field.onBlur(e);
-        }}
+// https://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
+const formatSeparator = amount => amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+class InputField extends React.Component {
+  format = value => value && formatSeparator(value);
+
+  unformat = value => value && value.toString().replace(/[,]/g, '');
+
+  render() {
+    const { name, validate, isNumeric, ...inputProps } = this.props;
+
+    return (
+      <Field
+        name={name}
+        validate={validate}
+        render={({ field, form: { errors, touched } }) => (
+          <Input
+            displayType={errors[name] ? 'error' : 'default'}
+            message={errors[name] && touched[name] ? errors[name] : ''}
+            {...inputProps}
+            {...field}
+            onChange={(e) => {
+              if (inputProps.onChange) inputProps.onChange(e);
+              field.onChange(e);
+            }}
+            onBlur={(e) => {
+              if (inputProps.onBlur) inputProps.onBlur(e);
+              field.onBlur(e);
+            }}
+            formatter={isNumeric && { format: this.format, unformat: this.unformat }}
+          />
+        )}
       />
-    )}
-  />
-);
+    );
+  }
+}
 
 InputField.propTypes = {
   name: PropTypes.string.isRequired,
   validate: PropTypes.func,
+  /**
+   * isNumeric determines whether to format the number value or not
+   */
+  isNumeric: PropTypes.bool
+};
+
+InputField.defaultProps = {
+  isNumeric: false
 };
 
 export default InputField;
