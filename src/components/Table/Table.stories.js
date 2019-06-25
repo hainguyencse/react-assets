4a9f667a571639c1fef27ca08946c4534be989e0
Vu Nguyen <ngtrvu@gmail.com>
2019-06-25 18:14:29 +0700

    Update bootstrap, glyphicons, add listings to storybook

--- src/components/Table/Table.stories.js
+++ src/components/Table/Table.stories.js
@@ -2,20 +2,38 @@ import React from 'react';
 import { action } from '@storybook/addon-actions';
 import Table from '.';
 
-var data =  [
-              {
-                '#' : '1',
-                'First Name' : 'Mark',
-                'Last Name' : 'Otto',
-                'Username' : '@mdo'
-              },
-              {
-                '#' : '2',
-                'First Name' : 'Mark',
-                'Last Name' : 'Otto',
-                'Username' : '@mdo'
-              }
-            ];
+const data = [{
+  'id': 1,
+  'first_name': 'Mark',
+  'last_name': 'Otto',
+  'username': '@mdo'
+}, {
+  'id': 2,
+  'first_name': 'Mark',
+  'last_name': 'Otto',
+  'username': '@mdo'
+}, {
+  'id': 3,
+  'first_name': 'Mark',
+  'last_name': 'Otto',
+  'username': '@mdo'
+}, {
+  'id': 4,
+  'first_name': 'Mark',
+  'last_name': 'Otto',
+  'username': '@mdo'
+}, {
+  'id': 5,
+  'first_name': 'Mark',
+  'last_name': 'Otto',
+  'username': '@mdo'
+}, {
+  'id': 6,
+  'first_name': 'Mark',
+  'last_name': 'Otto',
+  'username': '@mdo'
+}
+];
 
 export default [
   {
